6c43bcb9af67d7fd9d05d3ee01534be2fe38511e
Helly Soni <helly.inexture@gmail.com>
2022-06-09 22:32:12 +0530

    settings update

--- BlogApp/settings.py
+++ BlogApp/settings.py
@@ -131,8 +131,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 STATICFILES_DIRS = (
