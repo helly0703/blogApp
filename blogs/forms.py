5e181c5a6b39e978fffc3c06dc77f58072a7b25a
Helly Soni <helly.inexture@gmail.com>
2022-07-07 17:49:20 +0530

    Blog Form corrected

--- blogs/forms.py
+++ blogs/forms.py
@@ -3,18 +3,17 @@ from blogs.models import Post, Category
 
 
 class BlogCreateForm(forms.ModelForm):
-    pass
-    # image = forms.ImageField(required=False)
-    #
-    # class Meta:
-    #     choices = Category.objects.all().values_list('name', 'name')
-    #     choice_list = []
-    #     for item in choices:
-    #         choice_list.append(item)
-    #
-    #     model = Post
-    #     fields = ['title', 'content', 'image', 'category']
-    #
-    #     widgets = {
-    #         'category': forms.Select(choices=choice_list,attrs={'class': 'form-control'}),
-    #     }
+    image = forms.ImageField(required=False)
+
+    class Meta:
+        choices = Category.objects.all().values_list('name', 'name')
+        choice_list = []
+        for item in choices:
+            choice_list.append(item)
+
+        model = Post
+        fields = ['title', 'content', 'image', 'category']
+
+        widgets = {
+            'category': forms.Select(choices=choice_list,attrs={'class': 'form-control'}),
+        }
