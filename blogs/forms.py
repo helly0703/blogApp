a06002de0c5c8903f404d0fee73485795f80d6f8
Helly Soni <helly.inexture@gmail.com>
2022-07-07 17:13:41 +0530

    implemented proper logic for chat feature

--- blogs/forms.py
+++ blogs/forms.py
@@ -3,17 +3,18 @@ from blogs.models import Post, Category
 
 
 class BlogCreateForm(forms.ModelForm):
-    image = forms.ImageField(required=False)
-
-    class Meta:
-        choices = Category.objects.all().values_list('name', 'name')
-        choice_list = []
-        for item in choices:
-            choice_list.append(item)
-
-        model = Post
-        fields = ['title', 'content', 'image', 'category']
-
-        widgets = {
-            'category': forms.Select(choices=choice_list,attrs={'class': 'form-control'}),
-        }
+    pass
+    # image = forms.ImageField(required=False)
+    #
+    # class Meta:
+    #     choices = Category.objects.all().values_list('name', 'name')
+    #     choice_list = []
+    #     for item in choices:
+    #         choice_list.append(item)
+    #
+    #     model = Post
+    #     fields = ['title', 'content', 'image', 'category']
+    #
+    #     widgets = {
+    #         'category': forms.Select(choices=choice_list,attrs={'class': 'form-control'}),
+    #     }
