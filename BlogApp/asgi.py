8ae888496b19129ede77e51c9ac3f2ad988e38e9
Helly Soni <helly.inexture@gmail.com>
2022-07-08 12:35:44 +0530

    update asgi

--- BlogApp/asgi.py
+++ BlogApp/asgi.py
@@ -1,17 +1,23 @@
 import os
-import chat.routing
+
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BlogApp.settings')
+django_asgi_app = get_asgi_application()
+
+import chat.routing
+from channels.auth import AuthMiddlewareStack
 
 application = ProtocolTypeRouter({
-    'https': get_asgi_application(),
-    'websocket': AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                chat.routing.websocket_urlpatterns
+            )
         )
-    )
-    # Just HTTP for now. (We can add other protocols later.)
+    ),
 })
